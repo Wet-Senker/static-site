67bc87b4c0f413b636b75edea760fc6556841c78
Wet-Senker <dhrnickdevries@hotmail.com>
2025-07-03 21:13:43 +0200

    returns markdown Blocktype

--- src/blocks.py
+++ src/blocks.py
@@ -1,11 +1,35 @@
 from textnode import TextNode, TextType
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph" 
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
-    list_of_blocks = markdown.split("\n")
-    stripped_list = []
-    for entry in list_of_blocks:
-        stripped_entry = entry.strip()
-        if stripped_entry:
-            stripped_list.append(stripped_entry)
-    print(stripped_list)
-    return stripped_list
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+def block_to_block_type(markdown):
+   if markdown.startswith("#"):
+       return BlockType.HEADING
+   elif markdown.startswith("`"):
+        return BlockType.CODE
+   elif markdown.startswith(">"):
+        return BlockType.QUOTE
+   elif markdown.startswith("-"):
+        return BlockType.UNORDERED_LIST
+   elif markdown.startswith(r"\d+\."):
+        return BlockType.ORDERED_LIST 
+   else:
+       return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,21 +1,20 @@
 import unittest
 from blocks import (
     markdown_to_blocks,
+    block_to_block_type,
+    BlockType
 )
-
-from textnode import TextNode, TextType
-
-class TestMarkdownToBlocks(unittest.TestCase):
-  def test_markdown_to_blocks(self):
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_blocks(self):
         md = """
-    This is **bolded** paragraph
+This is **bolded** paragraph
 
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-    - This is a list
-    - with items
-    """
+- This is a list
+- with items
+"""
         blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
@@ -25,3 +24,43 @@ class TestMarkdownToBlocks(unittest.TestCase):
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+class TestBlockToBlock(unittest.TestCase):
+    def test_header_to_block(self):
+        result = block_to_block_type("# This is a header")
+        self.assertEqual(result, BlockType.HEADING)
+    
+    def test_second_header_to_block(self):
+        result = block_to_block_type("## This is a header")
+        self.assertEqual(result, BlockType.HEADING)
+    
+    def test_code_to_block(self):
+        result = block_to_block_type("`print(hoi)`")
+        self.assertEqual(result, BlockType.CODE)
+    
+    def test_quote_to_block(self):
+        result = block_to_block_type("> E=mc2")
+        self.assertEqual(result, BlockType.QUOTE)
